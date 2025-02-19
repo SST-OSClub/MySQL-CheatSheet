25c88cd0558c8f24d1a0cab354ca3445e36fb3c8
Soham Ambore <82763693+Soham124@users.noreply.github.com>
2025-02-19 22:04:24 +0530

    Soham Ambore 10258

---
+++ Soham10258.js
@@ -0,0 +1,83 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'SohamAmbore',
+  database: 'users'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting:', err);
+    return;
+  }
+  console.log('Connected as id', connection.threadId);
+});
+
+function createUser(name, email) {
+  const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+  connection.query(query, [name, email], (err, results) => {
+    if (err) {
+      console.error('Error inserting user:', err);
+      return;
+    }
+    console.log('User created with ID:', results.insertId);
+  });
+}
+
+function updateUserEmail(name, newEmail) {
+  const query = 'UPDATE users SET email = ? WHERE name = ?';
+  connection.query(query, [newEmail, name], (err, results) => {
+    if (err) {
+      console.error('Error updating user:', err);
+      return;
+    }
+    console.log(`User ${name} updated.`);
+  });
+}
+
+function deleteUser(name) {
+  const query = 'DELETE FROM users WHERE name = ?';
+  connection.query(query, [name], (err, results) => {
+    if (err) {
+      console.error('Error deleting user:', err);
+      return;
+    }
+    console.log(`User ${name} deleted.`);
+  });
+}
+
+function readUsers() {
+  const query = 'SELECT * FROM users';
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error reading users:', err);
+      return;
+    }
+    console.log('Users:', results);
+  });
+}
+
+function formIndex() {
+  const query = 'CREATE INDEX user_lookup ON users(name,email)';
+  connection.query(query, (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Index done');
+  });
+}
+
+
+createUser('Anurag Kumar', 'kumar.anurag@gmail.com');
+updateUserEmail('David Miller', 'david.miller_updated@example.com');
+deleteUser('Emily Clark');
+readUsers();
+formIndex();
+
+setTimeout(() => {
+  connection.end();
+  
+},2000);
